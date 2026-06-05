8878384d057e194d4e094397e4672919f5a88c97
uzairsaeedkhan <uzairsaeed120@gmail.com>
2026-06-05 10:08:18 +0200

    updated api client file while integration

--- src/api_client.py
+++ src/api_client.py
@@ -1,26 +1,26 @@
 import requests
+from src.logger import get_logger
+logger = get_logger(__name__)
+
 class CountryLeadersAPI:
-    def __init__(self):
+    def __init__(self, session: requests.Session):
         self.base_url: str = "https://country-leaders.onrender.com"
         self.country_endpoint: str = f"{self.base_url}/countries"
         self.leaders_endpoint: str = f"{self.base_url}/leaders"
         self.cookies_endpoint: str = f"{self.base_url}/cookie"
-        self.session: requests.Session = requests.Session()
+        self.session = session  # use the shared session instead of creating a new one
 
     def refresh_cookies(self):
-        # Logic to refresh cookies
+        logger.info("Refreshing cookies...")
         response = self.session.get(self.cookies_endpoint)
         cookie_value = response.cookies.get('user_cookie')
-        self.session.cookies.set('user_cookie', cookie_value)
-        return self.session.cookies
+        self.session.cookies.set('user_cookie', cookie_value, domain='country-leaders.onrender.com')
+        logger.info("Cookies refreshed successfully")
 
-    def get_countries(self) -> list:
-        # Logic to get the list of countries
-        countries = self.session.get(self.country_endpoint).json()
-        return countries
+    def get_countries(self):
+        logger.info("Fetching countries...")
+        return self.session.get(self.country_endpoint).json()
 
-    def get_leaders(self, country: str) -> list:
-        # Logic to get the list of country leaders
-        leaders_url = f"{self.leaders_endpoint}?country={country}"
-        leaders = self.session.get(leaders_url).json()
-        return leaders
+    def get_leaders(self, country):
+        logger.info(f"Fetching leaders for {country}...")
+        return self.session.get(f"{self.leaders_endpoint}?country={country}").json()
